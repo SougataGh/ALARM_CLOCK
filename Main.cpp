c5e8bc3f23a7a11b03ae8df544a7d76e10f4663b
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-04 16:25:33 +0530

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+int main() {
+    int hr, mn, sc;
+    cout << "Enter the time in format HH : MM : SS -\n";
+    cin >> hr >> mn >> sc;
+    alrmClock c1(hr, mn, sc);
+    int ahr, amn, asc;
+    cout << "Set the Alarm [HH : MM : SS] -\n";
+    cin >> ahr >> amn >> asc;
+    c1.setAlarm(ahr, amn, asc);
+    c1.displayClock();
+}
