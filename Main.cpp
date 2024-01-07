6a7522cb0871b15ea0e88dd9b15209a85db031ae
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-07 09:50:06 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,7 @@ int main() {
     cin >> hr >> mn >> sc;
     alrmClock c1(hr, mn, sc);
     int ahr, amn, asc;
-    cout << "Set the Alarm [HH : MM : SS] -\n";
+    cout << "Set the Alarm [HH : MM : SS] ( 24 HOURS FORMAT )-\n";
     cin >> ahr >> amn >> asc;
     c1.setAlarm(ahr, amn, asc);
     c1.displayClock();
