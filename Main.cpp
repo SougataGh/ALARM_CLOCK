16d9dd4408b61bfe9dadf414327c148b5da26124
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-07 09:49:25 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 int main() {
     int hr, mn, sc;
-    cout << "Enter the time in format HH : MM : SS -\n";
+    cout << "Enter the time in format HH : MM : SS ( 24 HOURS FORMAT )-\n";
     cin >> hr >> mn >> sc;
     alrmClock c1(hr, mn, sc);
     int ahr, amn, asc;
